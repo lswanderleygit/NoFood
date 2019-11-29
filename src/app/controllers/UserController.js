f9a8e69c8a813a4d1d4c1e13e0872f498c936c7b
Lucas de Sousa Wanderley <lucaswanderley4@gmail.com>
2019-11-29 11:22:43 -0300

    add validations to user controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,12 +1,13 @@
 "use strict";
 
 const Yup = require("yup");
+const md5 = require("md5");
 
 const UserModel = require("../models/UserModel");
 
 class UserController {
   async index(req, res) {
-    const listUser = await UserModel.find();
+    const listUser = await UserModel.find({}, "name email image");
 
     if (!listUser) {
       return res.status(400).json({ message: "Users not found" });
@@ -15,16 +16,30 @@ class UserController {
     res.status(200).send(listUser);
   }
 
+  async authenticate(req, res) {
+    const { email, password } = await req.body;
+    const hashPassword = md5(password);
+
+    const auth = UserModel.findOne(
+      { email, password: hashPassword },
+      "name email _id"
+    );
+
+    if (auth) {
+    }
+  }
+
   async show(req, res) {
     const schema = Yup.string().required();
 
     if (await schema.isValid(req.params.id)) {
-      const userFound = await UserModel.findById(req.params.id);
-      return res.status(200).send(userFound);
+      const { email, _id, image } = await UserModel.findById(req.params.id);
+      return res.status(200).send({ email, _id, image });
     }
 
     res.status(400).json({ error: "Id is required" });
   }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
@@ -34,18 +49,31 @@ class UserController {
       password: Yup.string()
         .min(6)
         .required(),
+      confirmPassword: Yup.string().required(),
       image: Yup.string(),
       active: Yup.boolean().required()
     });
+
+    if (req.body.password != req.body.confirmPassword) {
+      return res.status(400).json({ message: "Passwords does not match" });
+    }
+
+    const checkUserExists = await UserModel.findOne({ email: req.body.email });
+
+    if (checkUserExists) {
+      return res.status(400).json({ message: "Email already in use." });
+    }
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: "Validation fails" });
     }
 
-    const user = await UserModel.create(req.body);
+    req.body.password = md5(req.body.password);
+    const { name, email, _id } = await UserModel.create(req.body);
 
-    res.status(201).json(user);
+    res.status(201).json({ name, email, _id });
   }
+
   async update(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
@@ -62,12 +90,25 @@ class UserController {
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: "Validation fails" });
     }
+    const { name, email, image, password } = req.body;
 
-    const userFound = await UserModel.findOneAndUpdate(req.params.id, {
-      $set: req.body
-    });
+    const emailExists = await UserModel.findOne({ email });
 
-    res.status(202).json(userFound);
+    if (emailExists) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const userUpdate = await UserModel.update(
+      { _id: req.params.id },
+      {
+        name,
+        email,
+        image,
+        password
+      }
+    );
+
+    res.status(202);
   }
 
   async delete(req, res) {
