3d1eb3b339b9db706f87948459e909628e1b5959
Lucas de Sousa Wanderley <lucaswanderley4@gmail.com>
2019-12-02 16:50:37 -0300

    add route register and add auth to all routes, finish api

--- src/routes.js
+++ src/routes.js
@@ -9,24 +9,26 @@ const auth = require("./middlewares/auth");
 const routes = new Router();
 
 // Rotues category
-routes.get("/api/category", CategoryController.index);
-routes.get("/api/category/:id", CategoryController.show);
-routes.post("/api/category", CategoryController.store);
-routes.put("/api/category/:id", CategoryController.update);
-routes.delete("/api/category/:id", CategoryController.delete);
+routes.post("/api/category", auth, CategoryController.store);
+routes.get("/api/category", auth, CategoryController.index);
+routes.get("/api/category/:id", auth, CategoryController.show);
+routes.put("/api/category/:id", auth, CategoryController.update);
+routes.delete("/api/category/:id", auth, CategoryController.delete);
 
 // Routes product
-routes.get("/api/product", ProductController.index);
-routes.get("/api/product/:id", ProductController.show);
-routes.post("/api/product", ProductController.store);
-routes.put("/api/product/:id", ProductController.update);
-routes.delete("/api/product/:id", ProductController.delete);
+routes.post("/api/product", auth, ProductController.store);
+routes.get("/api/product", auth, ProductController.index);
+routes.get("/api/product/:id", auth, ProductController.show);
+routes.put("/api/product/:id", auth, ProductController.update);
+routes.delete("/api/product/:id", auth, ProductController.delete);
 
 // Routes User
+routes.post("/api/user/register", UserController.store);
 routes.post("/api/user/session", UserController.session);
+
+routes.post("/api/user", auth, UserController.store);
 routes.get("/api/user", auth, UserController.index);
 routes.get("/api/user/:id", auth, UserController.show);
-routes.post("/api/user", UserController.store);
 routes.put("/api/user/:id", auth, UserController.update);
 routes.delete("/api/user/:id", auth, UserController.delete);
 
